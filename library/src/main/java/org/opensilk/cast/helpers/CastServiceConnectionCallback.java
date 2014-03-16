59e781c6b6f9c0355ac5f72e2e5537eef834c14e
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2014-03-16 16:00:43 -0500

    fix copyright

--- library/src/main/java/org/opensilk/cast/helpers/CastServiceConnectionCallback.java
+++ library/src/main/java/org/opensilk/cast/helpers/CastServiceConnectionCallback.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) $date.year OpenSilk Productions LLC
+ * Copyright (C) 2014 OpenSilk Productions LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
